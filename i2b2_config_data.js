f96743b597f73b9a52cf09521d6fac14034b2cbe
Janice Donahoe <Janice@phs000774.partners.org>
2015-09-14 08:19:28 -0400

    initial install

---
+++ i2b2_config_data.js
@@ -0,0 +1,15 @@
+{
+	urlProxy: "index.php",
+	urlFramework: "js-i2b2/",
+	//-------------------------------------------------------------------------------------------
+	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
+	lstDomains: [
+		{ domain: "i2b2demo",
+		  name: "HarvardDemo",
+		  urlCellPM: "http://webservices.i2b2.org/i2b2/services/PMService/",
+		  allowAnalysis: true,
+		  debug: false
+		}
+	]
+	//-------------------------------------------------------------------------------------------
+}
