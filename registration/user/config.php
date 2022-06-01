d957906836b8d2ff9f9bf33634ea6a15431699f1
Michael Mendis <mmendis@partners.org>
2022-06-01 12:03:36 -0400

    fixed url for pm

--- registration/user/config.php
+++ registration/user/config.php
@@ -3,6 +3,6 @@
 $config_service_account_id = 'i2b2';
 $config_service_account_pw = 'demouser';
 
-$config_pm_uri = 'https://127.0.0.1:9090/webclient/i2b2/services/PMService/getServices';
+$config_pm_uri = 'http://127.0.0.1:9090/i2b2/services/PMService/getServices';
 $config_domain = 'i2b2demo';
 $config_project_id = 'Demo';
