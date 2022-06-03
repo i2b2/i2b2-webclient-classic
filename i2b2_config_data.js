882b7df171cd08dfddf0eab42db49ad728639f83
mem61 <mem61@192.168.6.146>
2022-06-03 14:51:09 -0400

    Added sample config js file

---
+++ i2b2_config_data.js
@@ -0,0 +1,39 @@
+{
+	urlProxy: "index.php",
+	urlFramework: "js-i2b2/",
+	startZoomed: true,
+	//-------------------------------------------------------------------------------------------
+	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
+	lstDomains: [
+          //      {
+          //              name: "i2b2demo_local",
+          //              domain: "i2b2demo",
+          //              urlCellPM: "http://127.0.0.1:9090/i2b2/services/PMService/",
+          //              allowAnalysis: true,
+          //              registrationMethod: "local",
+          //              loginType: "local",
+          //              showRegistration: true,
+          //              debug: true
+          //      }
+          //      ,
+          //      {
+          //              name: "i2b2demo_saml",
+          //              domain: "i2b2demo",
+          //              urlCellPM: "http://127.0.0.1:9090/i2b2/services/PMService/",
+          //              allowAnalysis: true,
+          //              registrationMethod: "saml",
+          //              loginType: "federated",
+          //              showRegistration: true,
+          //              debug: true
+          //      }
+          //      ,				{ 
+						name: "i2b2 Demo",
+						domain: "i2b2demo",
+						urlCellPM: "http://services.i2b2.org/i2b2/services/PMService/",
+						allowAnalysis: false,
+						debug: true,
+						isSHRINE: false
+				}
+	]
+	//-------------------------------------------------------------------------------------------
+}
